a7a3a1b33c1a4a82471a7872e0f10c1c4f4b4b54
Alejandro Alvarez <avazquez@liquibase.com>
2024-10-09 15:57:31 +0200

    Update test.sql

--- test.sql
+++ test.sql
@@ -2,3 +2,6 @@
 
 --changeset your.name:1
 CREATE TABLE test_table (test_id INT NOT NULL, test_column INT, PRIMARY KEY (test_id))
+
+--changeset your.name:2
+CREATE TABLE test_table2 (test_id INT NOT NULL, test_column INT, PRIMARY KEY (test_id))
